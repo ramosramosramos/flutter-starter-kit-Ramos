0d779d077c02cb30c82e6c33a180571d1b646aa4
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-03-07 17:25:18 +0800

    profile components done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter_kit/layouts/auth_layout.dart';
+import 'package:flutter_starter_kit/pages/auth/login.dart';
 
 void main() {
   runApp(MaterialApp(
     theme: ThemeData.dark(),
     debugShowCheckedModeBanner: false,
-    home: AuthLayout(),
+    home: Login(),
   ));
 }
 
